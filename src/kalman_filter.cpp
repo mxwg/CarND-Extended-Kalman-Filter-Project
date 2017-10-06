960e5e0a80b99e9c53adcd5436dcc6f3579be141
Maximilian Wenger <mwenger@uos.de>
2017-10-06 16:07:37 +0200

    Working version

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,7 +1,10 @@
 #include "kalman_filter.h"
+#include <iostream>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
+using std::endl;
+using std::cout;
 
 KalmanFilter::KalmanFilter() {}
 
@@ -22,6 +25,10 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+    x_ = F_ * x_; // + u
+
+    // update covariance
+    P_ = F_ * P_ * F_.transpose() + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -29,6 +36,18 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+    Eigen::VectorXd z_pred = H_ * x_;
+    Eigen::VectorXd y = z - z_pred;
+    Eigen::MatrixXd Ht = H_.transpose();
+    Eigen::MatrixXd S = H_ * P_ * Ht + R_;
+    Eigen::MatrixXd Si = S.inverse();
+    Eigen::MatrixXd PHt = P_ * Ht;
+    Eigen::MatrixXd K = PHt * Si;
+
+    //new estimate
+    x_ = x_ + (K * y);
+    Eigen::MatrixXd I = Eigen::MatrixXd::Identity(x_.size(), x_.size());
+    P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -36,4 +55,34 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+    double px = x_(0);
+    double py = x_(1);
+    double vx = x_(2);
+    double vy = x_(3);
+    // map state to radar measurement
+    double rho = sqrt(px*px + py*py);
+    double theta = atan2(py, px);
+    double rhoDot = (px*vx+py*vy)/rho;
+
+    Eigen::VectorXd h_radar(3);
+    h_radar << rho, theta, rhoDot;
+
+    Eigen::VectorXd y = z - h_radar;
+    y(1) = atan2(sin(y(1)), cos(y(1))); // normalize angle
+
+//    cout << "======" << endl;
+//    cout << "z: " << z.transpose() << endl;
+//    cout << "h: " << h_radar.transpose() << endl;
+//    cout << "y: " << y.transpose() << endl;
+//    cout << "======" << endl;
+
+    Eigen::MatrixXd Ht = H_.transpose();
+    Eigen::MatrixXd S = H_ * P_ * Ht + R_;
+    Eigen::MatrixXd Si = S.inverse();
+    Eigen::MatrixXd PHt = P_ * Ht;
+    Eigen::MatrixXd K = PHt * Si;
+
+    x_ = x_ + (K * y);
+    Eigen::MatrixXd I = Eigen::MatrixXd::Identity(x_.size(), x_.size());
+    P_ = (I - K * H_) * P_;
 }
--- src/tools.cpp
+++ src/tools.cpp
@@ -11,15 +11,67 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+    if (estimations.size() != ground_truth.size() || estimations.size() < 1)
+    {
+        throw std::runtime_error("Input sizes are incorrect!");
+    }
+
+    VectorXd rmse(4);
+    rmse << 0,0,0,0;
+
+    //accumulate squared residuals
+    for(int i=0; i < estimations.size(); ++i){
+        // ... your code here
+        for(size_t j=0; j < 4; ++j)
+        {
+            double diff = estimations[i][j] - ground_truth[i][j];
+            rmse[j] += (diff * diff);
+        }
+    }
+
+    for(size_t j = 0; j < 4; ++j)
+    {
+        rmse[j] = rmse[j] / estimations.size();
+    }
+    for(size_t j = 0; j < 4; ++j)
+    {
+        rmse[j] = sqrt(rmse[j]);
+    }
+
+    return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+    if(x_state.size() != 4)
+    {
+        throw std::runtime_error("Jacobian can only be calculated for a state of size 4!");
+    }
+    MatrixXd Hj(3,4);
+    Hj.setZero();
+    //recover state parameters
+    float px = x_state(0);
+    float py = x_state(1);
+    float vx = x_state(2);
+    float vy = x_state(3);
+
+    //TODO: YOUR CODE HERE
+
+    double one = px*px + py*py;
+    double two = sqrt(one);
+    double three = one * two;
+
+    //check division by zero
+    if (one < 0.00001)
+    {
+        std::cout << "Division by zero." << std::endl;
+        return Hj;
+    }
+
+    //compute the Jacobian matrix
+    Hj << px/two, py/two, 0, 0,
+            -py/one, px/one, 0, 0,
+            py*(vx*py-vy*px)/three, px*(vy*px-vx*py)/three, px/two, py/two;
+
+//  std::cout << Hj << std::endl;
+    return Hj;
 }
